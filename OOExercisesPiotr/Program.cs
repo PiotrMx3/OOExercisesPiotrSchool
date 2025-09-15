f9ef9e51122f19927a530ce943453c116326a1ab
Piotr Mulinski <piotr.mulinski@icloud.com>
2025-09-15 19:07:05 +0200

    Add project files.

---
+++ OOExercisesPiotr/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace OOExercisesPiotr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welkom bij de oefeningen van Objectgeoriënteerd Programmeren!");
+            Console.WriteLine("Topic van de uit te voeren oefening?");
+            Console.WriteLine("1: DateTime");
+
+
+            string k = Console.ReadLine() ?? "".Trim();
+
+            switch (k)
+            {
+                case "1":
+                    _DateTime.ShowSubmenu();
+                    break;
+
+                default:
+                    Console.WriteLine("verkeerde keuze");
+                    break; 
+
+            }
+
+            
+        }
+        
+    }
+}
---
+++ OOExercisesPiotr/_DateTime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOExercisesPiotr
+{
+    internal class _DateTime
+    {
+        public static void ShowSubmenu()
+        {
+
+            Console.WriteLine("Uit te voeren oefening?");
+            Console.WriteLine("H10-Clock");
+            Console.WriteLine("H10-Birthday");
+            Console.WriteLine("H10-DayOfTheWeek");
+            Console.WriteLine("H10-TicksSince2000");
+            Console.WriteLine("H10-LeapYearCount");
+            Console.WriteLine("H10-CodeTiming");
+            Console.WriteLine("H10-CodeTiming");
+
+            string k = Console.ReadLine() ?? "".Trim();
+
+            switch (k)
+            {
+                case "1":
+                    //TODO: H10-Clock
+                    break;
+                default:
+                    Console.WriteLine("Verkeerd keuze !");
+                    break;
+            }
+
+
+        }
+    }
+}
